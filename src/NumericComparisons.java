52d13c5f2926c758bf7868ad81370e7a40a2fa7a
kangbo1229 <kangbo@ssfs.org>
2019-10-22 13:50:09 -0400

    Completed NumericComparisons

--- src/NumericComparisons.java
+++ src/NumericComparisons.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class NumericComparisons {
 
-    public static boolean isOdd(int num1){
+    public static boolean isOdd(int num){
 
-        return num1 % 2 != 0;
+        return num % 2 != 0;
         }
 
         public static void main(String[] args) {
@@ -17,31 +17,46 @@ public class NumericComparisons {
             System.out.println(num + " is negative: " + isNegative(num));
             System.out.println(num + " is nonnegative: " + isNonNegative(num));
             System.out.println(num + " is nonpositive: " + isNonPositive(num));
+            System.out.print("Enter the number: ");
+            Scanner kb = new Scanner(System.in);
+            int num2 = kb.nextInt();
+            System.out.println(num + " is Factor of " + num2 + ": " + isFactor(num,num2));
+            System.out.println(num + " is Multiple of " + num2 + ": " + isMultiple(num,num2));
         }
 
-        public static boolean isZero(int num2) {
+        public static boolean isZero(int num) {
 
-        return num2 == 0;
+        return num == 0;
         }
 
-        public static boolean isPositive(int num3){
+        public static boolean isPositive(int num){
 
-        return num3 > 0;
+        return num > 0;
         }
 
-        public static boolean isNegative(int num4){
+        public static boolean isNegative(int num){
 
-        return num4 < 0;
+        return num < 0;
         }
 
-        public static boolean isNonNegative(int num5){
+        public static boolean isNonNegative(int num){
 
-        return num5 >= 0;
+        return num >= 0;
         }
 
-        public static boolean isNonPositive(int num6){
+        public static boolean isNonPositive(int num){
 
-        return num6 <= 0;
+        return num <= 0;
+        }
+
+        public static boolean isFactor(int num, int num2){
+
+        return num2%num == 0;
+        }
+
+        public static boolean isMultiple(int num, int num2){
+
+        return num%num2 == 0;
         }
 
 }
