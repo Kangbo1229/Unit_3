bce5fde7616f033ac6cff5bea1586596c678c0e4
kangbo1229 <kangbo@ssfs.org>
2019-10-29 11:21:01 -0400

    w

---
+++ src/TrickyDice.java
@@ -0,0 +1,13 @@
+public class TrickyDice {
+    public static int regularRoll() {
+        int normal = (int)(Math.random()*6)+1;
+        return normal;
+
+    }
+}
+    public static int trickyRoll() {
+
+    public static void main(String[] args) {
+
+    }
+}
