2ae8dfb538d1d6cc7405251538b481f0a80e34f8
kangbo1229 <kangbo@ssfs.org>
2019-11-01 15:13:41 -0400

    d

---
+++ src/CookieOrders.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CookieOrders {
+    public static String totalCost(int numBoxes) {
+        if (numBoxes>0 && numBoxes<5 ) {
+           return
+        }
+        if (numBoxes>4 && numBoxes<10) {
+            return
+        }
+        if (numBoxes>9 && numBoxes<16) {
+            return
+        }
+        if (numBoxes >= 16) {
+            return
+        }
+
+    }
+    public static void main(String[] args){
+        System.out.println("Enter the number of boxes you wisj to purchase: ");
+        Scanner scan = new Scanner(System.in);
+        int boxes = scan.nextInt();
+        System.out.println("You bought "+)
+
+    }
+}
