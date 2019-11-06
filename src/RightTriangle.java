6e5d59516e59e9db2c3aaa4e4b1600261dc93e2d
kangbo1229 <kangbo@ssfs.org>
2019-11-06 08:55:17 -0500

    w

--- src/RightTriangle.java
+++ src/RightTriangle.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class RightTriangle {
+    public static boolean isTriangle(int a, int b, int c) {
+        if (a+b>c) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public static boolean isRight(int a, int b, int c) {
+        int
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter three integer sides of a triangle: ");
+        int sides = scan.nextInt();
+        System.out.println(isRight(sides));
+    }
+
 }
