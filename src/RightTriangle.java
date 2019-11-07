ea24f6bc4aa1de761f37ac7b2b9ae289cff03218
kangbo1229 <kangbo@ssfs.org>
2019-11-07 10:14:47 -0500

    w

--- src/RightTriangle.java
+++ src/RightTriangle.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class RightTriangle {
     public static boolean isTriangle(int a, int b, int c) {
-        if (a+b>c) {
+        if (a+b>c && a+c>b && b+c>a) {
             return true;
         }
         else {
@@ -10,13 +10,20 @@ public class RightTriangle {
         }
     }
     public static boolean isRight(int a, int b, int c) {
-        int
+        boolean one = Math.pow(a,2) + Math.pow(b,2) == Math.pow(c,2);
+        boolean two = Math.pow(a,2) + Math.pow(c,2) == Math.pow(b,2);
+        boolean three = Math.pow(c,2) + Math.pow(b,2) == Math.pow(a,2);
+        return one || two || three;
     }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter three integer sides of a triangle: ");
-        int sides = scan.nextInt();
-        System.out.println(isRight(sides));
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        if ()
+        System.out.println("The side lengths of "+a+" , "+b+" , and "+c+" form a right triangle");
+
     }
 
 }
